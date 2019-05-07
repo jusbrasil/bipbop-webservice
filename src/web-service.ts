127a7658f2ae5ec791cd3d9d0cefdacb37ad8cff
Lucas Fernando Amorim <lf.amorim@yahoo.com.br>
2019-05-07 20:38:37 -0300

    documentação - beta

--- src/web-service.ts
+++ src/web-service.ts
@@ -3,20 +3,44 @@ import fetch from 'cross-fetch';
 import querystring from 'querystring';
 import managerPromise from 'promise-limit';
 
+/**
+ * Chave de API livre ao público para comunicação anônima
+ */
 const defaultApiKey: string = '6057b71263c21e4ada266c9d4d4da613';
 
-export type Configs = {[conf: string]: string};
 export type Form = {[conf: string]: string};
 
 type FormPrepare = (data: Form | FormData) => FormData | null;
 
-
+/**
+ * A classe WebService é responsável para que aplicações possam
+ * se comunicar com a BIPBOP com facilidade usando o esquema fetch
+ * https://developer.mozilla.org/pt-BR/docs/Web/API/Fetch_API/Using_Fetch
+ * 
+ * ```ts
+ * import WebService from 'bipbop-webservice'
+ * const maxClients: number = 10;
+ * const fetchOptions: RequestInit = {}; // https://developer.mozilla.org/pt-BR/docs/Web/API/Fetch_API/Using_Fetch
+ * const webService: WebService = new WebService('bipbop-apikey', maxClients, fetchOptions);
+ * const response: Response = await webService.request("SELECT FROM 'INFO'.'INFO'");
+ * const responseBody: string = await response.text();
+ * console.log(responseBody);
+ * ```
+ */
 export default class WebService {
-	public apiKey: string;
 	public configs: RequestInit;
-  public bipbopLimit: ((fn: () => Promise<Response>) => Promise<Response>)
+  public apiKey: string;
 
-  constructor(apiKey: string = defaultApiKey, userLimit: number | null = 5, configs: Configs = {}) {
+  private bipbopLimit: ((fn: () => Promise<Response>) => Promise<Response>)
+
+  /**
+   * Inicializa uma consulta 
+   * 
+   * @param apiKey Chave de API da BIPBOP (https://www.bipbop.com.br)
+   * @param userLimit Quantidade máxima de consultas simultâneas
+   * @param configs Configurações do fetch (https://developer.mozilla.org/pt-BR/docs/Web/API/Fetch_API/Using_Fetch)
+   */
+  constructor(apiKey: string = defaultApiKey, userLimit: number | null = 5, configs: RequestInit = {}) {
     let limit = userLimit !== null ? (userLimit > 10 ? 10 : userLimit) : 10;
     this.bipbopLimit = managerPromise<Response>(limit);
     this.apiKey = apiKey;
@@ -43,6 +67,13 @@ export default class WebService {
     }];
   }
 
+  /**
+   * Realiza uma requisição
+   * 
+   * @param query Consulta a BIPBOP (ex.: SELECT FROM 'INFO'.'INFO')
+   * @param form Parâmetros da consulta - POST
+   * @param urlData Parâmetros da consulta - GET
+   */
   request(query: string, form: Form | FormData = {}, urlData: Form = {}) : Promise<Response> {
     return this.bipbopLimit(() => fetch(...this.object(query, form, urlData)));
   }
