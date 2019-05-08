b51940a994ff2348d37b56b3e3dc03f8d5edd508
Lucas Fernando Amorim <lf.amorim@yahoo.com.br>
2019-05-08 10:13:04 -0300

    feature: remove push

--- src/browser.ts
+++ src/browser.ts
@@ -1,14 +1,10 @@
 import WebService from './web-service';
-import Push from './push';
-import PushParameters from './push';
 import Exception from './exception';
 import Exceptions from './exception';
 import ErrorCodes from './error-codes';
 
 export {
     WebService,
-    Push,
-    PushParameters,
     ErrorCodes,
     Exception,
     Exceptions,
